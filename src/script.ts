e112b40ef42772d5052723f0a4e2474fa904a6de
Victor Hosth <kvbn147@gmail.com>
2023-01-21 19:41:34 +0000

    Upgrade Responsividade

--- src/script.ts
+++ src/script.ts
@@ -4,7 +4,7 @@ const MenuMobile = document.querySelector('.menuMobile') as HTMLElement;
 
 const menuUl = document.querySelector('.container_menu ul') as HTMLElement
 
-MenuMobile.addEventListener('click', function(){
+MenuMobile.addEventListener('click', function () {
     MenuMobile.classList.toggle('active')
     menuUl.classList.toggle('active')
 })
@@ -16,27 +16,15 @@ MenuMobile.addEventListener('click', function(){
 const elementSkiils = document.querySelectorAll('.space_language .item')
 
 
-type skiils = {order: number, name: string, percent: number}
+type skiils = { order: number, name: string, percent: number }
 
 let skills: skiils[] = [
-    {   order: 0,
-        name: "html",
-        percent: 90 },
-    {   order: 1,
-        name: "css",
-        percent: 90 },
-    {   order: 2,
-        name: "JavasScript",
-        percent: 70 },
-    {   order: 3,
-        name: "TypeScript",
-        percent: 60 },
-    {   order: 4,
-        name: "Git",
-        percent: 80 },
-    {   order: 5,
-        name: "NodeJS",
-        percent: 20 },
+    { order: 0, name: "html", percent: 90 },
+    { order: 1, name: "css", percent: 90 },
+    { order: 2, name: "JavasScript", percent: 70 },
+    { order: 3, name: "TypeScript", percent: 60 },
+    { order: 4, name: "Git", percent: 80 },
+    { order: 5, name: "NodeJS", percent: 20 }
 ];
 function colocarPortcentagem() {
     for (let x = 0; x < elementSkiils.length; x++) {
@@ -45,7 +33,6 @@ function colocarPortcentagem() {
         item.style.width = (percent + "%");
     }
 }
-colocarPortcentagem();
 colocarPortcentagem()
 
 
